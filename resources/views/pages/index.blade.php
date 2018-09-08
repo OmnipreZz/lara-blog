b43e594c6382efe433acffe5e559c6e7fbb579c1
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-09-08 14:06:15 +0200

    update delete only if author

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,26 +2,31 @@
 
 @section('content')
 <div class="container">
-@foreach ($posts as $post)
-<div class="card mb-5">
-  <img class="card-img-top" src="" alt="Card image cap">
-  <div class="card-header text-center">
-   {{ $post->title }}
-   @foreach($post->tags as $tag)
-   <span class="badge badge-primary">{{$tag->name}}</span>
-   @endforeach
-  </div>
-  <div class="card-body">
+  @foreach ($posts as $post)
+  <div class="card mb-5">
+    <img class="card-img-top" src="" alt="Card image cap">
+    <div class="card-header text-center">
+     {{ $post->title }}
+     @foreach($post->tags as $tag)
+     <span class="badge badge-primary">{{$tag->name}}</span>
+     @endforeach
+   </div>
+   <div class="card-body">
     <p class="card-text">{{ $post->content }}</p>
   </div>
   <div class="card-footer">
     <p class="card-text">Posted by {{ $post->author }} at {{ $post->created_at}}</p>
+    @auth
+    @if ($post->author ==  Auth::user()->name )
     <a href="/post/delete/{{$post->id}}" class="btn btn-danger" role="button">Delete</a>
     <a href="/post/update/{{$post->id}}" class="btn btn-primary" role="button">Update</a>
-    <a href="/post/show/{{$post->id}}" class="btn btn-success" role="button">Comment</a>
+    @endif
+    @endauth
+    <a href="/post/show/{{$post->id}}" class="btn btn-success" role="button">Show</a>
   </div>
 </div>
 @endforeach
 </div>
 
 @endsection
+
